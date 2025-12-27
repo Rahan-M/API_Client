c50e059791b7ab1c446d2fc7d4071fe24176f50f
Rahan-M <rahan01729@gmail.com>
2025-12-28 01:35:35 +0530

    fix: snackbar error

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,11 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SnackbarProvider>
+    <SnackbarProvider
+      maxSnack={3}
+      autoHideDuration={3000}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+    >
       <App />
     </SnackbarProvider>
   </StrictMode>,
